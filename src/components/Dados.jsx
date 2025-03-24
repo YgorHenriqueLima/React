e3a8f1db33194b99cf3ac8c987866974f6fac8cf
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-03-24 17:07:11 -0300

    terminando modificações hoje

---
+++ src/components/Dados.jsx
@@ -0,0 +1,9 @@
+export default function Dados(props){
+    return(
+        <section>
+            <p>Canal: {props.canal} </p>
+            <p>youtube: {props.youtube} </p>
+            <p>curso: {props.curso} </p>
+        </section>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,14 @@
 import './App.css';
+import Dados from './Dados';
 import paisagem1 from './imgs/paisagem-1.jpg';
 import paisagem2 from './imgs/paisagem-2.jpg';
 
 export default function Main(){
-
-    const canal = ()=>{
-        return("cfb cursos");
-    }
-    
-    const curso = "Curso de React";
-    
     return(
         <main className='conteudo'>  
             <img src={paisagem1} alt="paisagem1"/>
             <img src={paisagem2} alt="paisagem2"/>
-            <p>canal {canal()}</p>
-            <p>fazendo o {curso}</p>       
+            <Dados canal='CFBcursos' youtube='www.youtube.com' curso='curso de react'/>   
         </main>
     )
 }
